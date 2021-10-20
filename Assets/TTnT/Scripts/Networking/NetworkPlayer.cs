c66545dc6560236ea2f4ab4928633851867d8817
termi17385 <66066987+termi17385@users.noreply.github.com>
2021-10-20 13:57:59 +1100

    UI Updates

--- Assets/TTnT/Scripts/Networking/NetworkPlayer.cs
+++ Assets/TTnT/Scripts/Networking/NetworkPlayer.cs
@@ -1,5 +1,6 @@
 using Mirror;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace TTnT.Scripts.Networking
 {
@@ -7,15 +8,28 @@ namespace TTnT.Scripts.Networking
     public class NetworkPlayer : NetworkBehaviour
     {
         [SerializeField] private Camera cam;
+        [SerializeField] private MouseLook mouseLook;
+        [SerializeField] private Canvas canvas;
+        [SerializeField] private UIFollowCam followCam;
+        [SerializeField] private Transform target;
+        [FormerlySerializedAs("ui"),SerializeField] private GameObject selfUI;
   
         // Update is called once per frame
         void Update()
         {
             if (isLocalPlayer)
             {
-                
+                target = transform;
+                selfUI.SetActive(false);
             }
-            else cam.enabled = false;
+            else
+            {
+                cam.enabled = false;
+                canvas.enabled = false;
+                mouseLook.enabled = false;
+                followCam.enabled = false;
+                followCam.target = target;
+            }
         }
 
         public override void OnStartClient()
